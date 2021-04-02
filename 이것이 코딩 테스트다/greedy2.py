206ebe53d536a5f77e6a4f35289895f71a0fe3c3
aeddung <48937254+aeddung@users.noreply.github.com>
2021-04-02 17:22:35 +0900

    큰 수의 법칙
    입력 부분에서 공백이 포함된 여러 개 문자를 받는 부분이 어색했다. map()과 split() 함수를 익히자.
    큰 수부터 k 번 더하고, for문 종료되면 두 번째 큰 수를 더한 뒤 다시 for문 반복한다.
    m이 10,000 이상일 때를 고려하여 코드에 수학식을 적용해본다. 손으로 풀면서 반복 패턴을 찾아보는 연습을 하자.

--- 이것이 코딩 테스트다/greedy2.py
+++ 이것이 코딩 테스트다/greedy2.py
@@ -28,3 +28,22 @@ while True:
   m -= 1
 
 print(result)
+
+# m이 10,000 이상일 때 위의 방법으로 구현하려면 시간 초과
+# 수학적으로 계산해보자.(반복되는 패턴을 찾아보기)
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+# 가장 큰 수가 더해지는 횟수
+# int(m / (k + 1))은 특정한 수열 형태가 반복되는 횟수
+# ex) 6 + 6 + 6 + 5 / 6 + 6 + 6 + 5 / 6 
+# m이 k+1에 의해 나누어 떨어지지 않는 경우 나머지를 더해준다.
+num_of_first = int(m / (k + 1)) * k + (m % (k + 1))
+
+result = num_of_first * first + int(m / (k + 1)) * second # int(m / (k + 1)) = m - num_of_first
+
+print(result)
