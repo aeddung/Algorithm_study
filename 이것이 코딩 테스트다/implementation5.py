6c55b21f8b994b91c8a9bbc08a64ba2df7e18653
aeddung <48937254+aeddung@users.noreply.github.com>
2021-04-20 17:03:29 +0900

    문자열 재정렬
    isalpha() 함수와 isnumber() 함수를 처음(?) 알았다기 보단 거의 사용해본 적이 없으니 당황스러웠다.
    이외에도 isascii(), isdigit(), isnummeric() 함수가 있으며 문자열만 사용 가능한 함수인 듯 하다.

---
+++ 이것이 코딩 테스트다/implementation5.py
@@ -0,0 +1,43 @@
+"""
+알파벳 대문자와 숫자(0~9)로만 구성된 문자열이 입력으로 주어짐
+모든 알파벳을 오름차순으로 정렬하고 이어서 모든 숫자를 더한 형태를 출력
+"""
+# 본인이 작성한 코드
+s = input()
+
+alpha = []
+num = []
+for i in s:
+  if i.isalpha():
+    alpha.append(i)
+  else:
+    num.append(i)
+
+alpha.sort()
+summary = 0
+for i in num:
+  summary += int(i)
+
+alpha = ''.join(alpha) + str(summary)
+print(alpha)
+
+##########################################################################################
+# 책 예시 코드
+data = input()
+result = []
+value = 0
+
+# 문자를 하나씩 확인
+for x in data:
+  if x.isalpha():
+    result.append(x)
+  else:
+    value += int(x)
+    
+result.sort()
+# 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+if value != 0:
+  result.append(str(value))
+  
+# 리스트를 문자열로 변환하여 출력
+print(''.join(result))
