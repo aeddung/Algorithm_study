d4e82867d72fc141848f610a1e4df4c129a3bdb7
jun8 <48937254+aeddung@users.noreply.github.com>
2021-10-01 13:46:24 +0900

    상위 n개 레코드
    멋도 모르고 서브쿼리를 사용한 인라인 뷰를 사용했던 코드다.
    공부 차원에서 괜찮지만 사실 더 간단하게 해결 가능하다.

--- programmers_solution/SQL/상위 n개 레코드.sql
+++ programmers_solution/SQL/상위 n개 레코드.sql
@@ -1,6 +1,13 @@
+-- 문제 보기(https://moondol-ai.tistory.com/282?category=921465)
+
 -- 본인이 작성한 쿼리문
 SELECT A.NAME FROM(
 SELECT * FROM ANIMAL_INS
 ORDER BY DATETIME
 ) AS A
+LIMIT 1;
+
+-- 참고 쿼리문
+SELECT NAME FROM ANIMAL_INS
+ORDER BY DATETIME
 LIMIT 1;
