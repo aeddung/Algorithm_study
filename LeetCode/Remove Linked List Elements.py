6dfd7c25a876dc490487b7ca9f4e16f1b7ec62e5
jun8 <48937254+aeddung@users.noreply.github.com>
2022-10-03 20:35:53 +0900

    Remove Linked List Elements
    파라미터로 주어진 val에 해당하지 않는 숫자들만 Linked List를 구성해서 리턴하면 된다.

---
+++ LeetCode/Remove Linked List Elements.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        dummy.next = head
+        curr = dummy
+
+        while curr.next is not None:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return dummy.next
