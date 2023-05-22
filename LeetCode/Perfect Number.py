3200b050b6e9fc9c4d4f2f12d104ce70df16715a
jun8 <48937254+aeddung@users.noreply.github.com>
2023-05-22 21:55:19 +0900

    Perfect Number.py
    이전에 풀었던 문제와 비슷한 로직으로 주어진 원소의 거듭제곱근을 취해 해당 숫자까지만 loop를 돌리면 된다.

---
+++ LeetCode/Perfect Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+          
+        res = 1
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                res += i + num // i
+        
+        if res == num:
+            return True
+        return False
